be663e082a506c4fbbe759aed70da060a4c973cc
OlhaIrhizova <irhizovaolha@gmail.com>
2024-10-14 15:53:47 +0300

    hw4js

--- js/task-1.js
+++ js/task-1.js
@@ -7,14 +7,10 @@ function isEnoughCapacity(products, containerSize){
     for(const key of keys){
      total += products[key];
     }
-    if(total <= containerSize){
-      return true;
-    }else{
-      return false;
-    }
     
+      return total <= containerSize;
     
-    }
+    };
     
     console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
     console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
